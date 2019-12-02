ed6e5cfe46497bb03e73d2585856927dedd3caa0
james <hagenjh@gmail.com>
2019-12-02 12:59:44 -0700

    changes

--- Java.js
+++
@@ -1,8 +0,0 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: {lat: 41.878114, lng: -87.629798}
-  });
-
-  var transitLayer = new google.maps.TransitLayer();
-  transitLayer.setMap(map);
